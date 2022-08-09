fbc4145919d7711c75fa1c661be5bfe884ea6f77
Pirmagomedov <58191115+Pirmagomedov@users.noreply.github.com>
2022-08-09 23:56:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express'),
   app = express(),
-  http = require('http').createServer(app)
+  http = require('http').createServer(app),
+  io = require('socket.io')(http)
 
 const host = '127.0.0.1'
 const port = 7000
@@ -8,7 +9,8 @@ const port = 7000
 let clients = []
 
 io.on('connection', (socket) => {
-  console.log(`sdfsdf`)
+  console.log(`Client with id ${socket.id} connected`)
+  clients.push(socket.id)
 
   socket.emit('message', "I'm server")
 
@@ -16,10 +18,16 @@ io.on('connection', (socket) => {
     console.log('Message: ', message)
   )
 
-  
+  socket.on('disconnect', () => {
+    clients.splice(clients.indexOf(socket.id), 1)
+    console.log(`Client with id ${socket.id} disconnected`)
+  })
 })
 
- 
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => res.render('index'))
+
 //получение количества активных клиентов
 app.get('/clients-count', (req, res) => {
   res.json({
@@ -28,8 +36,23 @@ app.get('/clients-count', (req, res) => {
 })
 
 //отправка сообщения конкретному клиенту по его id
- 
- 
+app.post('/client/:id', (req, res) => {
+  if (clients.indexOf(req.params.id) !== -1) {
+    io.sockets.connected[req.params.id].emit(
+      'private message',
+      `Message to client with id ${req.params.id}`
+    )
+    return res
+      .status(200)
+      .json({
+        message: `Message was sent to client with id ${req.params.id}`,
+      })
+  } else
+    return res
+      .status(404)
+      .json({ message: 'Client not found' })
+})
+
 http.listen(port, host, () =>
   console.log(`Server listens http://${host}:${port}`)
 )
