fc150fe6e6b43c0c81ff6a2145c57f364cbe790c
Pirmagomedov <58191115+Pirmagomedov@users.noreply.github.com>
2022-08-10 00:09:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
 
-const server = new WebSocket.Server({port: 3000;});
+const server = new WebSocket.Server({port: 3000});
 
 server.on("connection", ws => {
   ws.send("ergegergrtg");
