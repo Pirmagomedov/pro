7384cfcee9124aec2f1b4460c8064acecd36f482
Pirmagomedov <58191115+Pirmagomedov@users.noreply.github.com>
2022-08-10 02:25:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 const WebSocket = require("ws");
 
-const server = new WebSocket.Server({port: 3000});
+const wsServer = new WebSocket.Server({port: 9000});
 
-server.on("connection", ws => {
-  ws.send("ergegergrtg");
-});
+wsServer.on("connection", onConnect);
+
+function onConnect (wsClient){
+  console.log("NeW uSeR !");
+  wsClient.send("HeLlO !");
+  
+  wsClient.on("message", function(message){
+    try{
+      const jsonMessage = JSON.parse(message);
+      switch (jsonMessage) {
+        case "ECHO":
+          wsClient.send(jsonMessage.data);
+          break;
+        case "PING":
+          setTimeout(function() {
+            wsClient.send("PONG");
+          }, 2000)
+          break;
+        default:
+          console.log("CoMmAnD BaD !");
+          break;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  wsClient.on("close", function(){
+    console.log("UsEr dEcOnNeCtEd !");
+  });
+
+}
+
+console.log("9000 ----------- !!!!!!!!!");
