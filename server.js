4ffa5eec99650f3f50c1ee4e590436aeb087ed6e
Pirmagomedov <58191115+Pirmagomedov@users.noreply.github.com>
2022-08-10 19:39:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,36 +2,14 @@ const WebSocket = require("ws");
 
 const wsServer = new WebSocket.Server({port: 9000});
 
-var list = [];
-
 wsServer.on("connection", onConnect);
 
 function onConnect (wsClient){
   console.log("NeW uSeR !");
-  list.push(wsClient);
-  list.forEach(function(item){
-    item.send("HELOOOOOOOOO");
-  });
   
   wsClient.on("message", function(message){
-    try{
-      const jsonMessage = JSON.parse(message);
-      switch (jsonMessage) {
-        case "ECHO":
-          wsClient.send(jsonMessage.data);
-          break;
-        case "PING":
-          setTimeout(function() {
-            wsClient.send("PONG");
-          }, 2000)
-          break;
-        default:
-          console.log("CoMmAnD BaD !");
-          break;
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    const jsonMessage = message;
+    wsClient.send(jsonMessage);
   });
   
   wsClient.on("close", function(){
