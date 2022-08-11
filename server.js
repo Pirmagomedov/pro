949ef56a2d0542d1731402ebbc67a61d9e0ba96a
Pirmagomedov <58191115+Pirmagomedov@users.noreply.github.com>
2022-08-11 15:28:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,44 @@
 const WebSocket = require("ws");
-const createClient = require('redis');
+
+
+
+
+var redis = require('redis');
+
+var client = redis.createClient()
+
+
+
+
+
+client.set('myKey', 'Hello Redis', function (err, repl) {
+    if (err) {
+           // Оо что то случилось при записи
+           console.log('Что то случилось при записи: ' + err);
+           client.quit();
+    } else {
+           // Прочтем записанное
+           client.get('myKey', function (err, repl) {
+                   //Закрываем соединение, так как нам оно больше не нужно
+                   client.quit();
+                   if (err) {
+                           console.log('Что то случилось при чтении: ' + err);
+                   } else if (repl) {
+                   // Ключ найден
+                           console.log('Ключ: ' + repl);
+               } else {
+                   // Ключ ненайден
+                   console.log('Ключ ненайден.')
+
+           };
+           });
+    };
+});
+
+
+
+
+
 
 const wsServer = new WebSocket.Server({port: 9000});
 
