6d05b1d7fb4db7aad68332ccb72c52c018235856
Pirmagomedov <58191115+Pirmagomedov@users.noreply.github.com>
2022-08-11 16:01:17 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,38 +14,17 @@ wsServer.on("connection", onConnect);
 
 function onConnect (wsClient){
     
+    
+    
+    
+    
+    
+    
     client.get('myKey', function (err, repl) {
        console.log(repl);  
     });
     
-    ////////////////
     
-    /*client.set('myKey', 'Бла бла-бла бла', function (err, repl) {
-    if (err) {
-           // Оо что то случилось при записи
-           console.log('Что то случилось при записи: ' + err);
-           client.quit();
-    } else {
-           // Прочтем записанное
-           client.get('myKey', function (err, repl) {
-                   //Закрываем соединение, так как нам оно больше не нужно
-                   client.quit();
-                   if (err) {
-                           console.log('Что то случилось при чтении: ' + err);
-                   } else if (repl) {
-                   // Ключ найден
-                           console.log('Ключ: ' + repl);
-               } else {
-                   // Ключ ненайден
-                   console.log('Ключ ненайден.')
-
-                        };
-                    });
-                };
-            }); */
-
-    
-    /////////////////
     
     
   clients.push(wsClient);
